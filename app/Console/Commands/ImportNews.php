958b6831cf58e01cf6dc56aa749a83c3190f2a99
andre-aguayo <andre.aguayo@hotmail.com>
2022-05-30 05:46:53 -0400

    Criaçao do comando para importaçao de noticias

---
+++ app/Console/Commands/ImportNews.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\News;
+use App\Services\NewsService;
+use Illuminate\Console\Command;
+
+class ImportNews extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:import-news';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command used to import news every day';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->comment("Importins news... ");
+
+        $newService = new NewsService();
+        $newService->importNews();
+
+        $this->info("Import succefull.");
+        return 1;
+    }
+}
